aba8f580028e66c163d793ecc860926cf6a11b5c
Alborz Geramifard <agf@csail.mit.edu>
2013-02-14 18:05:11 +0000

    errs should exclude 0 size

--- TEST.py
+++ TEST.py
@@ -1,13 +1,7 @@
-from sklearn import svm
 from Tools import *
 from Domains import *
 from Representations import *
 
-M = array([[1,2,3,4],
-           [5,6,7,8]])
-n = 2
-A = array([1,0],dtype=integer)
-A = kron(A,ones((1,n,),dtype=integer))[0]
-print A 
-print M[A,arange(len(A)),:].reshape(-1)
+X = os.path.getsize('TEST.py')
+print X
 
