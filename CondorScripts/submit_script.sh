ce34045d0073bdd1581190d83ac04d46d9b6a0b3
Alborz Geramifard <agf@csail.mit.edu>
2013-02-27 00:13:27 +0000

    4GB mem for submission

--- CondorScripts/submit_script.sh
+++ CondorScripts/submit_script.sh
@@ -8,16 +8,15 @@ priority = 0
 Notification = Never
 # yodel machines have no scipy
 
-requirements = OpSys == "LINUX"
-#Requirements = Machine != "yodel7.csail.mit.edu" && Machine != "yodel4.csail.mit.edu" && Machine != "yodel2.csail.mit.edu" && Machine != "yodel10.csail.mit.edu" && Machine != "yodel7.csail.mit.edu" && Machine != "yodel6.csail.mit.edu" && Machine != "yodel8.csail.mit.edu" && Machine != "yodel17.csail.mit.edu" && Machine != "yodel13.csail.mit.edu" && Machine != "yodel12.csail.mit.edu" && Machine != "yodel11.csail.mit.edu"
+# 2GB MEMORY REQUIREMENT
+requirements = OpSys == "LINUX" && Memory >= 2048
 
+#Requirements = Machine != "yodel7.csail.mit.edu" && Machine != "yodel4.csail.mit.edu" && Machine != "yodel2.csail.mit.edu" && Machine !=
 #Requirements = isPublic && \
 #               Memory >= 6144 && \
 #               Cpus >= 1 && \
-
 #Requirements = Arch == "X86_64"
-# 2GB MEMORY REQUIREMENT
-Requirements = Memory >= 2048
+
 Error = CondorOutput/err/$(PROCESS).err
 Log = CondorOutput/log/$(PROCESS).log
 Output = CondorOutput/out/$(PROCESS).out
