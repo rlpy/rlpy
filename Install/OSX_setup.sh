ecab2faa218e7bec9359b46c1ef6827933cb2ce2
Robert H. Klein <rhklein@mit.edu>
2013-04-03 07:09:16 +0000

    Updated ubuntu/mac installation ---MAC INSTALLER STILL NEEDS TESTING -removed latex -set RL_PYTHON_ROOT environment variable

--- Install/OSX_setup.sh
+++ Install/OSX_setup.sh
@@ -56,19 +56,16 @@ if [ $EXIT_CODE -eq 0 ]; then
     echo -e "Both numpy and scipy have been detected.  Installation will proceed.\n"
 fi
 
-
-# TODO: Install / check for installation of Tex fonts on OSX, if possible
-
 echo -e "Now installing setuptools, required for package installation using pip."
 curl -O http://python-distribute.org/distribute_setup.py
 sudo python distribute_setup.py
 
-echo -e "\n Now installing pip"
+echo -e "\n Now installing pip."
 sudo easy_install pip
-echo -e "pip installation complete"
+echo -e "pip installation complete."
 
 echo -e "\nBeginning installation of required packages.\n\n"
-echo -e "Now installing swig, required for scipy"
+echo -e "Now installing swig, required for scipy."
 brew install swig
 #sudo pip install numpy
 #sudo pip install scipy
@@ -76,7 +73,6 @@ brew install swig
 sudo pip install networkx
 # nose is used to run unit tests on packages
 #sudo pip install nose
-
 
 # TODO: Scikit learn also requires setuptools, some sites say no further update req'd.
 # Must test below on fresh system. 
@@ -104,6 +100,85 @@ echo "scikit-learn==0.12.1"
 echo "scipy==0.11.0"
 
 echo ""
-echo "Note that this script does not install required latex files"
+
+read -p "Ready to continue to final step of installation? (Press [Enter])"
+clear
+
+# Final step adds a line to .bash_profile to source the file RL_Python_setup.bash,
+# after the user locates the install directory.
+# The file RL_Python_setup.bash is included in the repository, but we have
+# commented code here to automatically regenerate it.
+#
+# TODO Test: if the environment variable is not properly set after testing,
+# modify a plist instead: http://stackoverflow.com/questions/7501678/set-environment-variables-on-mac-os-x-lion#
+
+# Set the environment variable RL_PYTHON_ROOT
+echo -e "We need to set an environment variable for the location of the RL-Python"
+echo -e "project directory.\nIt appears to be located in:\n"
+cd ..
+pwd
+cd - > /dev/null
+# Above suppresses output of cd - command, which returns to previous directory.
+
+echo -e "\nIs this correct? (And where you intend to continue working from?)\n [Yes or No]"
+read yes_no
+INSTALL_PATH="null"
+VALID_DIRECTORY_ZERO="1" # Start with improper directory
+while [ "$VALID_DIRECTORY_ZERO" -ne 0 ]
+do
+    case $yes_no in
+        Yes) cd ..
+             INSTALL_PATH=`pwd`
+             cd - > /dev/null; VALID_DIRECTORY_ZERO="0"
+             ;;
+        No)  echo -e "Please enter the absolute path to the RL-Python root directory: "
+             read INSTALL_PATH
+             cd $INSTALL_PATH
+             VALID_DIRECTORY_ZERO=$?
+             ;;
+    esac
+    if [ $VALID_DIRECTORY_ZERO -eq 0 ]; then
+        echo -e "\nValid directory specified. "
+#        echo -e "The file RL_Python_setup.bash will be created.\n"
+    else
+        echo -e "\nYou specified an invalid directory; maybe you haven't created it yet?\n"
+        # Automatically force entry of python path in loop above
+        yes_no="No"
+    fi
+done
 echo ""
-echo "Installation script complete."
+cd $INSTALL_PATH
+#echo "Creating file RL_Python_setup.bash in directory $INSTALL_PATH"
+# sudo echo 'export RL_PYTHON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" ' > RL_Python_setup.bash
+
+# Make a backup of the .bashrc file before editing!
+cd ~
+sudo cp .bash_profile .bash_profile_RL_PYTHON_BACKUP
+
+# Determine if .bashrc already sources this file in some way
+ALREADY_EXPORTED=`find ~ -name '.bash_profile' -exec grep RL_Python_setup.bash {} +`
+
+# A file is already sourced from bash_profile
+if [ "$?" -eq 0 ]; then
+    echo -e "Your .bash_profile file already appears to source RL_Python_setup.bash;"
+    echo -e "this line will be overwritten with the newly created one based on"
+    echo -e "your answer above."
+    # Delete the line(s) containing 'RL_Python_setup.bash'
+    sed -i '/RL_Python_setup.bash/d' .bash_profile > /dev/null
+fi
+
+echo -e "\nAdding source of RL_Python_setup.bash to .bash_profile\n"
+
+#if [ "$?" -eq 0 ]; then
+    sudo echo "# Automatically added RL_Python_setup.bash below by OSX_setup.sh script for RL-Python" >> .bash_profile
+    sudo echo "source $INSTALL_PATH/RL_Python_setup.bash" >> .bash_profile
+    echo -e "Successfully modified .bash_profile\n"
+#else
+#    echo -e "There was a problem creating a backup of .bash_profile.  You will need \n"
+#    echo -e "to manually 'source' the file RL_Python_setup.bash for your shell\n"
+#    echo -e "environment; we recommend adding it to whatever startup script\n"
+#    echo -e "is used on your machine.\n"
+#fi
+
+echo -e "\n"
+read -p "Installation script complete, press [Enter] to exit."
--- Install/ubuntu_setup.sh
+++ Install/ubuntu_setup.sh
@@ -9,7 +9,7 @@ echo ""
 echo Your Python version:
 python --version
 VALID_VERSION=`python -c 'import sys; print("%i" % (0x020700F0 <= sys.hexversion<0x03000000))'`
-if [ $VALID_VERSION -eq 0 ]; then
+if [ "$VALID_VERSION" -eq 0 ]; then
     echo -e "Please install Python 2.7 <= Version < 3 before proceeding, and ensure"
     echo -e "that you only have one active copy of Python on your PATH variable.\n"
     echo -e "If you suspect competing installations of Python, see http://stackoverflow.com/questions/7746240/in-bash-which-gives-an-incorrect-path-python-versions\n"
@@ -20,20 +20,21 @@ else
 fi
 
 
-
-echo "Do you wish to install latex fonts as well, required for some visualizations?"
-select yes_no in "Yes" "No";
-do
-    case $yes_no in
-        Yes ) sudo apt-get install texlive-latex-extra texlive-fonts-recommended; echo -e "\nInstallation of Tex fonts Complete.\n"; break;;
-        No ) echo -e "\nUser opted to ignore addons.\n"; break;;
-    esac
-done
+# No longer require latex fonts, matplotlib is capable of displaying equations.
+#
+#echo "Do you wish to install latex fonts as well, required for some visualizations?"
+#select yes_no in "Yes" "No";
+#do
+#    case $yes_no in
+#        Yes ) sudo apt-get install texlive-latex-extra texlive-fonts-recommended; echo -e #"\nInstallation of Tex fonts Complete.\n"; break;;
+#        No ) echo -e "\nUser opted to ignore addons.\n"; break;;
+#    esac
+#done
 
 echo -e "\nBeginning installation of required packages.\n\n"
 sudo apt-get install python-dev python-setuptools python-numpy python-scipy python-matplotlib python-networkx graphviz
 
-echo -e "\nDo you want to install the package scikit-learn as well?"
+echo -e "\nDo you want to install the package scikit-learn as well? (Highly recommended, required for Pendulum domain and BEBF representation)"
 select yes_no in "Yes" "No";
 do
     case $yes_no in
@@ -44,17 +45,98 @@ done
 echo ""
 
 echo "The status of optional packages is shown below:"
-dpkg -s texlive-latex-extra |  grep Status
-dpkg -s texlive-fonts-recommended |  grep Status
-dpkg -s libatlas-dev |  grep Status
+#dpkg -s texlive-latex-extra |  grep Status
+#dpkg -s texlive-fonts-recommended |  grep Status
+dpkg -s libatlas-dev |  grep 'Package\|Status';
 
-echo -e "\n\nThe status of all required packages are shown below:"
-dpkg -s python-dev |  grep Status
-dpkg -s python-setuptools |  grep Status
-dpkg -s python-numpy |  grep Status
-dpkg -s python-scipy |  grep Status
-dpkg -s python-matplotlib |  grep Status
-dpkg -s graphviz |  grep Status
-dpkg -s python-networkx |  grep Status
+echo -e "\n\nThe statuses of all required packages are shown below:"
+dpkg -s python-dev |  grep 'Package\|Status';
+dpkg -s python-setuptools |  grep 'Package\|Status';
+dpkg -s python-numpy |  grep 'Package\|Status';
+dpkg -s python-scipy |  grep 'Package\|Status';
+dpkg -s python-matplotlib |  grep 'Package\|Status';
+dpkg -s graphviz |  grep 'Package\|Status';
+dpkg -s python-networkx |  grep 'Package\|Status';
 
-echo -e "\n\nInstallation script complete."
+echo -e "\nIf any required packages are shown as missing, try re-running this script;"
+echo -e "otherwise attempt sudo apt-get install <<missing package>> manually.\n"
+
+read -p "Ready to continue to final step of installation? (Press [Enter])"
+clear
+
+# Final step adds a line to .bashrc to source the file RL_Python_setup.bash,
+# after the user locates the install directory.
+# The file RL_Python_setup.bash is included in the repository, but we have
+# commented code here to automatically regenerate it.
+
+# Set the environment variable RL_PYTHON_ROOT
+echo -e "We need to set an environment variable for the location of the RL-Python"
+echo -e "project directory.\nIt appears to be located in:\n"
+cd ..
+pwd
+cd - > /dev/null
+# Above suppresses output of cd - command, which returns to previous directory.
+
+echo -e "\nIs this correct? (And where you intend to continue working from?)\n [Yes or No]"
+read yes_no
+INSTALL_PATH="null"
+VALID_DIRECTORY_ZERO="1" # Start with improper directory
+while [ "$VALID_DIRECTORY_ZERO" -ne 0 ]
+do
+    case $yes_no in
+        Yes) cd ..
+             INSTALL_PATH=`pwd`
+             cd - > /dev/null; VALID_DIRECTORY_ZERO="0"
+             ;;
+        No)  echo -e "Please enter the absolute path to the RL-Python root directory: "
+             read INSTALL_PATH
+             cd $INSTALL_PATH
+             VALID_DIRECTORY_ZERO=$?
+             ;;
+    esac
+    if [ $VALID_DIRECTORY_ZERO -eq 0 ]; then
+        echo -e "\nValid directory specified. "
+#        echo -e "The file RL_Python_setup.bash will be created.\n"
+    else
+        echo -e "\nYou specified an invalid directory; maybe you haven't created it yet?\n"
+        # Automatically force entry of python path in loop above
+        yes_no="No"
+    fi
+done
+echo ""
+cd $INSTALL_PATH
+#echo "Creating file RL_Python_setup.bash in directory $INSTALL_PATH"
+# sudo echo 'export RL_PYTHON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" ' > RL_Python_setup.bash
+
+# Make a backup of the .bashrc file before editing!
+cd
+sudo cp .bashrc .bashrc_RL_PYTHON_BACKUP
+
+# Determine if .bashrc already sources this file in some way
+ALREADY_EXPORTED=`find ~ -name '.bashrc' -exec grep RL_Python_setup.bash {} +`
+
+# A file is already sourced from bashrc
+if [ "$?" -eq 0 ]; then
+    echo -e "Your .bashrc file already appears to source RL_Python_setup.bash;"
+    echo -e "this line will be overwritten with the newly created one based on"
+    echo -e "your answer above."
+    # Delete the line(s) containing 'RL_Python_setup.bash'
+    sed -i '/RL_Python_setup.bash/d' .bashrc > /dev/null
+fi
+
+echo -e "\nAdding source of RL_Python_setup.bash to .bashrc\n"
+
+#if [ "$?" -eq 0 ]; then
+    sudo echo "# Automatically added RL_Python_setup.bash below by ubuntu_setup.sh script for RL-Python" >> .bashrc
+    sudo echo "source $INSTALL_PATH/RL_Python_setup.bash" >> .bashrc
+    echo -e "Successfully modified .bashrc\n"
+#else
+#    echo -e "There was a problem creating a backup of .bashrc.  You will need \n"
+#    echo -e "to manually 'source' the file RL_Python_setup.bash for your shell\n"
+#    echo -e "environment; we recommend adding it to whatever startup script\n"
+#    echo -e "is used on your machine.\n"
+#fi
+
+
+echo -e "\n"
+read -p "Installation script complete, press [Enter] to exit."
