6807ca03593e18d088b1e16b55e10dd81c90b31e
Robert H. Klein <rhklein@mit.edu>
2013-04-17 13:37:43 +0000

    ran doxygen on last updated files

--- Doxygen/Output/html/_representations.js
+++ Doxygen/Output/html/_representations.js
@@ -1,29 +1,4 @@
 var _representations =
 [
-    [ "Representation", "_representation.html", [
-      [ "Representation Superclass", "_representation.html#Representation_top", [
-        [ "Initialization", "_representation.html#Representation_init", null ],
-        [ "V", "_representation.html#Representation_V", null ],
-        [ "Qs", "_representation.html#Representation_Qs", null ],
-        [ "Q", "_representation.html#Representation_Q", null ],
-        [ "Phi", "_representation.html#Representation_phi", null ],
-        [ "Phi Sa", "_representation.html#Representation_phi_sa", null ],
-        [ "Add New Weight", "_representation.html#Representation_addNewWeight", null ],
-        [ "Hash State", "_representation.html#Representation_hashState", null ],
-        [ "Set Bins Per Dimension", "_representation.html#Representation_setBinsPerDimension", null ],
-        [ "Bin State", "_representation.html#Representation_binState", null ],
-        [ "Print All", "_representation.html#Representation_printAll", null ],
-        [ "Best Actions", "_representation.html#Representation_bestActions", null ],
-        [ "Best Action", "_representation.html#Representation_bestAction", null ],
-        [ "Phi Non Terminal", "_representation.html#Representation_phi_nonTerminal", null ],
-        [ "Active Initial Features", "_representation.html#Representation_activeInitialFeatures", null ],
-        [ "Batch Discover", "_representation.html#Representation_batchDiscover", null ],
-        [ "Batch Phi Sa", "_representation.html#Representation_batchPhi_s_a", null ],
-        [ "Batch Best Action", "_representation.html#Representation_batchBestAction", null ],
-        [ "Feature Type", "_representation.html#Representation_featureType", null ],
-        [ "Q One Step Look Ahead", "_representation.html#Representation_Q_oneStepLookAhead", null ],
-        [ "Qs One Step Look Ahead", "_representation.html#Representation_Qs_oneStepLookAhead", null ],
-        [ "V One Step Look Ahead", "_representation.html#Representation_V_oneStepLookAhead", null ]
-      ] ]
-    ] ]
+    [ "Representation", "_representation.html", null ]
 ];
--- Doxygen/Output/html/annotated.js
+++ Doxygen/Output/html/annotated.js
@@ -1,141 +1,47 @@
 var annotated =
 [
-    [ "Agents", null, [
-      [ "Agent", null, [
-        [ "Agent", "class_agents_1_1_agent_1_1_agent.html", "class_agents_1_1_agent_1_1_agent" ]
-      ] ],
-      [ "LSPI", null, [
-        [ "LSPI", "class_agents_1_1_l_s_p_i_1_1_l_s_p_i.html", "class_agents_1_1_l_s_p_i_1_1_l_s_p_i" ]
-      ] ],
-      [ "LSPI_SARSA", null, [
-        [ "LSPI_SARSA", "class_agents_1_1_l_s_p_i___s_a_r_s_a_1_1_l_s_p_i___s_a_r_s_a.html", "class_agents_1_1_l_s_p_i___s_a_r_s_a_1_1_l_s_p_i___s_a_r_s_a" ]
-      ] ],
-      [ "PolicyEvaluation", null, [
-        [ "PolicyEvaluation", "class_agents_1_1_policy_evaluation_1_1_policy_evaluation.html", "class_agents_1_1_policy_evaluation_1_1_policy_evaluation" ]
-      ] ],
-      [ "Q_LEARNING", null, [
-        [ "Q_LEARNING", "class_agents_1_1_q___l_e_a_r_n_i_n_g_1_1_q___l_e_a_r_n_i_n_g.html", "class_agents_1_1_q___l_e_a_r_n_i_n_g_1_1_q___l_e_a_r_n_i_n_g" ]
-      ] ],
-      [ "SARSA", null, [
-        [ "SARSA", "class_agents_1_1_s_a_r_s_a_1_1_s_a_r_s_a.html", "class_agents_1_1_s_a_r_s_a_1_1_s_a_r_s_a" ]
-      ] ]
-    ] ],
-    [ "Domains", null, [
-      [ "BlocksWorld", null, [
-        [ "BlocksWorld", "class_domains_1_1_blocks_world_1_1_blocks_world.html", "class_domains_1_1_blocks_world_1_1_blocks_world" ]
-      ] ],
-      [ "CartPole", null, [
-        [ "CartPole", "class_domains_1_1_cart_pole_1_1_cart_pole.html", "class_domains_1_1_cart_pole_1_1_cart_pole" ]
-      ] ],
-      [ "CartPole_InvertedBalance", null, [
-        [ "CartPole_InvertedBalance", "class_domains_1_1_cart_pole___inverted_balance_1_1_cart_pole___inverted_balance.html", "class_domains_1_1_cart_pole___inverted_balance_1_1_cart_pole___inverted_balance" ]
-      ] ],
-      [ "CartPole_SwingUp", null, [
-        [ "CartPole_SwingUp", "class_domains_1_1_cart_pole___swing_up_1_1_cart_pole___swing_up.html", "class_domains_1_1_cart_pole___swing_up_1_1_cart_pole___swing_up" ]
-      ] ],
-      [ "ChainMDP", null, [
-        [ "ChainMDP", "class_domains_1_1_chain_m_d_p_1_1_chain_m_d_p.html", "class_domains_1_1_chain_m_d_p_1_1_chain_m_d_p" ]
-      ] ],
-      [ "Domain", null, [
-        [ "Domain", "class_domains_1_1_domain_1_1_domain.html", "class_domains_1_1_domain_1_1_domain" ]
-      ] ],
-      [ "FiftyChain", null, [
-        [ "FiftyChain", "class_domains_1_1_fifty_chain_1_1_fifty_chain.html", "class_domains_1_1_fifty_chain_1_1_fifty_chain" ]
-      ] ],
-      [ "FlipBoard", null, [
-        [ "FlipBoard", "class_domains_1_1_flip_board_1_1_flip_board.html", "class_domains_1_1_flip_board_1_1_flip_board" ]
-      ] ],
-      [ "GridWorld", null, [
-        [ "GridWorld", "class_domains_1_1_grid_world_1_1_grid_world.html", "class_domains_1_1_grid_world_1_1_grid_world" ]
-      ] ],
-      [ "IntruderMonitoring", null, [
-        [ "IntruderMonitoring", "class_domains_1_1_intruder_monitoring_1_1_intruder_monitoring.html", "class_domains_1_1_intruder_monitoring_1_1_intruder_monitoring" ]
-      ] ],
-      [ "MountainCar", null, [
-        [ "MountainCar", "class_domains_1_1_mountain_car_1_1_mountain_car.html", "class_domains_1_1_mountain_car_1_1_mountain_car" ]
-      ] ],
-      [ "MultiTrack", null, [
-        [ "MultiTrack", "class_domains_1_1_multi_track_1_1_multi_track.html", "class_domains_1_1_multi_track_1_1_multi_track" ]
-      ] ],
-      [ "Pendulum", null, [
-        [ "Pendulum", "class_domains_1_1_pendulum_1_1_pendulum.html", "class_domains_1_1_pendulum_1_1_pendulum" ]
-      ] ],
-      [ "Pendulum_InvertedBalance", null, [
-        [ "Pendulum_InvertedBalance", "class_domains_1_1_pendulum___inverted_balance_1_1_pendulum___inverted_balance.html", "class_domains_1_1_pendulum___inverted_balance_1_1_pendulum___inverted_balance" ]
-      ] ],
-      [ "Pendulum_SwingUp", null, [
-        [ "Pendulum_SwingUp", "class_domains_1_1_pendulum___swing_up_1_1_pendulum___swing_up.html", "class_domains_1_1_pendulum___swing_up_1_1_pendulum___swing_up" ]
-      ] ],
-      [ "PitMaze", null, [
-        [ "PitMaze", "class_domains_1_1_pit_maze_1_1_pit_maze.html", "class_domains_1_1_pit_maze_1_1_pit_maze" ]
-      ] ],
-      [ "PST", null, [
-        [ "PST", "class_domains_1_1_p_s_t_1_1_p_s_t.html", "class_domains_1_1_p_s_t_1_1_p_s_t" ]
-      ] ],
-      [ "RCCar", null, [
-        [ "RCCar", "class_domains_1_1_r_c_car_1_1_r_c_car.html", "class_domains_1_1_r_c_car_1_1_r_c_car" ]
-      ] ],
-      [ "SystemAdministrator", null, [
-        [ "SystemAdministrator", "class_domains_1_1_system_administrator_1_1_system_administrator.html", "class_domains_1_1_system_administrator_1_1_system_administrator" ]
-      ] ]
-    ] ],
-    [ "Experiments", null, [
-      [ "BatchExperiment", null, [
-        [ "BatchExperiment", "class_experiments_1_1_batch_experiment_1_1_batch_experiment.html", "class_experiments_1_1_batch_experiment_1_1_batch_experiment" ]
-      ] ],
-      [ "Experiment", null, [
-        [ "Experiment", "class_experiments_1_1_experiment_1_1_experiment.html", "class_experiments_1_1_experiment_1_1_experiment" ]
-      ] ],
-      [ "OnlineExperiment", null, [
-        [ "OnlineExperiment", "class_experiments_1_1_online_experiment_1_1_online_experiment.html", "class_experiments_1_1_online_experiment_1_1_online_experiment" ]
-      ] ]
-    ] ],
-    [ "Policies", null, [
-      [ "eGreedy", null, [
-        [ "eGreedy", "class_policies_1_1e_greedy_1_1e_greedy.html", "class_policies_1_1e_greedy_1_1e_greedy" ]
-      ] ],
-      [ "FixedPolicy", null, [
-        [ "FixedPolicy", "class_policies_1_1_fixed_policy_1_1_fixed_policy.html", "class_policies_1_1_fixed_policy_1_1_fixed_policy" ]
-      ] ],
-      [ "Policy", null, [
-        [ "Policy", "class_policies_1_1_policy_1_1_policy.html", "class_policies_1_1_policy_1_1_policy" ]
-      ] ],
-      [ "UniformRandom", null, [
-        [ "UniformRandom", "class_policies_1_1_uniform_random_1_1_uniform_random.html", "class_policies_1_1_uniform_random_1_1_uniform_random" ]
-      ] ]
-    ] ],
-    [ "Representations", null, [
-      [ "BEBF", null, [
-        [ "BEBF", "class_representations_1_1_b_e_b_f_1_1_b_e_b_f.html", "class_representations_1_1_b_e_b_f_1_1_b_e_b_f" ]
-      ] ],
-      [ "Fourier", null, [
-        [ "Fourier", "class_representations_1_1_fourier_1_1_fourier.html", "class_representations_1_1_fourier_1_1_fourier" ]
-      ] ],
-      [ "iFDD", null, [
-        [ "iFDD_feature", "class_representations_1_1i_f_d_d_1_1i_f_d_d__feature.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d__feature" ],
-        [ "iFDD_potential", "class_representations_1_1i_f_d_d_1_1i_f_d_d__potential.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d__potential" ],
-        [ "iFDD", "class_representations_1_1i_f_d_d_1_1i_f_d_d.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d" ]
-      ] ],
-      [ "IncrementalTabular", null, [
-        [ "IncrementalTabular", "class_representations_1_1_incremental_tabular_1_1_incremental_tabular.html", "class_representations_1_1_incremental_tabular_1_1_incremental_tabular" ]
-      ] ],
-      [ "IndependentDiscretization", null, [
-        [ "IndependentDiscretization", "class_representations_1_1_independent_discretization_1_1_independent_discretization.html", "class_representations_1_1_independent_discretization_1_1_independent_discretization" ]
-      ] ],
-      [ "IndependentDiscretizationCompactBinary", null, [
-        [ "IndependentDiscretizationCompactBinary", "class_representations_1_1_independent_discretization_compact_binary_1_1_independent_discretization_compact_binary.html", "class_representations_1_1_independent_discretization_compact_binary_1_1_independent_discretization_compact_binary" ]
-      ] ],
-      [ "OMPTD", null, [
-        [ "OMPTD", "class_representations_1_1_o_m_p_t_d_1_1_o_m_p_t_d.html", "class_representations_1_1_o_m_p_t_d_1_1_o_m_p_t_d" ]
-      ] ],
-      [ "RBF", null, [
-        [ "RBF", "class_representations_1_1_r_b_f_1_1_r_b_f.html", "class_representations_1_1_r_b_f_1_1_r_b_f" ]
-      ] ],
-      [ "Representation", null, [
-        [ "Representation", "class_representations_1_1_representation_1_1_representation.html", "class_representations_1_1_representation_1_1_representation" ]
-      ] ],
-      [ "Tabular", null, [
-        [ "Tabular", "class_representations_1_1_tabular_1_1_tabular.html", "class_representations_1_1_tabular_1_1_tabular" ]
-      ] ]
-    ] ]
+    [ "Agent", "class_agents_1_1_agent_1_1_agent.html", "class_agents_1_1_agent_1_1_agent" ],
+    [ "BatchExperiment", "class_experiments_1_1_batch_experiment_1_1_batch_experiment.html", "class_experiments_1_1_batch_experiment_1_1_batch_experiment" ],
+    [ "BEBF", "class_representations_1_1_b_e_b_f_1_1_b_e_b_f.html", "class_representations_1_1_b_e_b_f_1_1_b_e_b_f" ],
+    [ "BlocksWorld", "class_domains_1_1_blocks_world_1_1_blocks_world.html", "class_domains_1_1_blocks_world_1_1_blocks_world" ],
+    [ "CartPole", "class_domains_1_1_cart_pole_1_1_cart_pole.html", "class_domains_1_1_cart_pole_1_1_cart_pole" ],
+    [ "CartPole_InvertedBalance", "class_domains_1_1_cart_pole___inverted_balance_1_1_cart_pole___inverted_balance.html", "class_domains_1_1_cart_pole___inverted_balance_1_1_cart_pole___inverted_balance" ],
+    [ "CartPole_SwingUp", "class_domains_1_1_cart_pole___swing_up_1_1_cart_pole___swing_up.html", "class_domains_1_1_cart_pole___swing_up_1_1_cart_pole___swing_up" ],
+    [ "ChainMDP", "class_domains_1_1_chain_m_d_p_1_1_chain_m_d_p.html", "class_domains_1_1_chain_m_d_p_1_1_chain_m_d_p" ],
+    [ "Domain", "class_domains_1_1_domain_1_1_domain.html", "class_domains_1_1_domain_1_1_domain" ],
+    [ "eGreedy", "class_policies_1_1e_greedy_1_1e_greedy.html", "class_policies_1_1e_greedy_1_1e_greedy" ],
+    [ "Experiment", "class_experiments_1_1_experiment_1_1_experiment.html", "class_experiments_1_1_experiment_1_1_experiment" ],
+    [ "FiftyChain", "class_domains_1_1_fifty_chain_1_1_fifty_chain.html", "class_domains_1_1_fifty_chain_1_1_fifty_chain" ],
+    [ "FixedPolicy", "class_policies_1_1_fixed_policy_1_1_fixed_policy.html", "class_policies_1_1_fixed_policy_1_1_fixed_policy" ],
+    [ "FlipBoard", "class_domains_1_1_flip_board_1_1_flip_board.html", "class_domains_1_1_flip_board_1_1_flip_board" ],
+    [ "Fourier", "class_representations_1_1_fourier_1_1_fourier.html", "class_representations_1_1_fourier_1_1_fourier" ],
+    [ "GridWorld", "class_domains_1_1_grid_world_1_1_grid_world.html", "class_domains_1_1_grid_world_1_1_grid_world" ],
+    [ "iFDD", "class_representations_1_1i_f_d_d_1_1i_f_d_d.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d" ],
+    [ "iFDD_feature", "class_representations_1_1i_f_d_d_1_1i_f_d_d__feature.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d__feature" ],
+    [ "iFDD_potential", "class_representations_1_1i_f_d_d_1_1i_f_d_d__potential.html", "class_representations_1_1i_f_d_d_1_1i_f_d_d__potential" ],
+    [ "IncrementalTabular", "class_representations_1_1_incremental_tabular_1_1_incremental_tabular.html", "class_representations_1_1_incremental_tabular_1_1_incremental_tabular" ],
+    [ "IndependentDiscretization", "class_representations_1_1_independent_discretization_1_1_independent_discretization.html", "class_representations_1_1_independent_discretization_1_1_independent_discretization" ],
+    [ "IndependentDiscretizationCompactBinary", "class_representations_1_1_independent_discretization_compact_binary_1_1_independent_discretization_compact_binary.html", "class_representations_1_1_independent_discretization_compact_binary_1_1_independent_discretization_compact_binary" ],
+    [ "IntruderMonitoring", "class_domains_1_1_intruder_monitoring_1_1_intruder_monitoring.html", "class_domains_1_1_intruder_monitoring_1_1_intruder_monitoring" ],
+    [ "LSPI", "class_agents_1_1_l_s_p_i_1_1_l_s_p_i.html", "class_agents_1_1_l_s_p_i_1_1_l_s_p_i" ],
+    [ "LSPI_SARSA", "class_agents_1_1_l_s_p_i___s_a_r_s_a_1_1_l_s_p_i___s_a_r_s_a.html", "class_agents_1_1_l_s_p_i___s_a_r_s_a_1_1_l_s_p_i___s_a_r_s_a" ],
+    [ "MountainCar", "class_domains_1_1_mountain_car_1_1_mountain_car.html", "class_domains_1_1_mountain_car_1_1_mountain_car" ],
+    [ "MultiTrack", "class_domains_1_1_multi_track_1_1_multi_track.html", "class_domains_1_1_multi_track_1_1_multi_track" ],
+    [ "OMPTD", "class_representations_1_1_o_m_p_t_d_1_1_o_m_p_t_d.html", "class_representations_1_1_o_m_p_t_d_1_1_o_m_p_t_d" ],
+    [ "OnlineExperiment", "class_experiments_1_1_online_experiment_1_1_online_experiment.html", "class_experiments_1_1_online_experiment_1_1_online_experiment" ],
+    [ "Pendulum", "class_domains_1_1_pendulum_1_1_pendulum.html", "class_domains_1_1_pendulum_1_1_pendulum" ],
+    [ "Pendulum_InvertedBalance", "class_domains_1_1_pendulum___inverted_balance_1_1_pendulum___inverted_balance.html", "class_domains_1_1_pendulum___inverted_balance_1_1_pendulum___inverted_balance" ],
+    [ "Pendulum_SwingUp", "class_domains_1_1_pendulum___swing_up_1_1_pendulum___swing_up.html", "class_domains_1_1_pendulum___swing_up_1_1_pendulum___swing_up" ],
+    [ "PitMaze", "class_domains_1_1_pit_maze_1_1_pit_maze.html", "class_domains_1_1_pit_maze_1_1_pit_maze" ],
+    [ "Policy", "class_policies_1_1_policy_1_1_policy.html", "class_policies_1_1_policy_1_1_policy" ],
+    [ "PolicyEvaluation", "class_agents_1_1_policy_evaluation_1_1_policy_evaluation.html", "class_agents_1_1_policy_evaluation_1_1_policy_evaluation" ],
+    [ "PST", "class_domains_1_1_p_s_t_1_1_p_s_t.html", "class_domains_1_1_p_s_t_1_1_p_s_t" ],
+    [ "Q_LEARNING", "class_agents_1_1_q___l_e_a_r_n_i_n_g_1_1_q___l_e_a_r_n_i_n_g.html", "class_agents_1_1_q___l_e_a_r_n_i_n_g_1_1_q___l_e_a_r_n_i_n_g" ],
+    [ "RBF", "class_representations_1_1_r_b_f_1_1_r_b_f.html", "class_representations_1_1_r_b_f_1_1_r_b_f" ],
+    [ "RCCar", "class_domains_1_1_r_c_car_1_1_r_c_car.html", "class_domains_1_1_r_c_car_1_1_r_c_car" ],
+    [ "Representation", "class_representations_1_1_representation_1_1_representation.html", "class_representations_1_1_representation_1_1_representation" ],
+    [ "SARSA", "class_agents_1_1_s_a_r_s_a_1_1_s_a_r_s_a.html", "class_agents_1_1_s_a_r_s_a_1_1_s_a_r_s_a" ],
+    [ "SystemAdministrator", "class_domains_1_1_system_administrator_1_1_system_administrator.html", "class_domains_1_1_system_administrator_1_1_system_administrator" ],
+    [ "Tabular", "class_representations_1_1_tabular_1_1_tabular.html", "class_representations_1_1_tabular_1_1_tabular" ],
+    [ "UniformRandom", "class_policies_1_1_uniform_random_1_1_uniform_random.html", "class_policies_1_1_uniform_random_1_1_uniform_random" ]
 ];
