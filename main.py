f5551ec1eb84ba3c016138f255e0bc541214a134
Alborz Geramifard <agf@csail.mit.edu>
2013-04-24 15:33:34 +0000

    Release of RLPy 1.0

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def main(jobID=-1,              # Used as an indicator for each run of the algor
 
     # Etc
     #----------------------
-    PERFORMANCE_CHECKS  = 10
-    LEARNING_STEPS      = 10000
+    PERFORMANCE_CHECKS  = 10 
+    LEARNING_STEPS      = 10000 # Max number of learning steps
     #EXPERIMENT_NAMING   = ['domain','agent','representation']
     EXPERIMENT_NAMING   = ['domain','representation','max_steps','representation.batchThreshold'] 
     EXPERIMENT_NAMING   = [] if not MAKE_EXP_NAME else EXPERIMENT_NAMING
