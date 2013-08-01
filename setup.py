6d660eac2270f5a58e17dd2c603160bfd4367e4a
Robert H. Klein <rhklein@mit.edu>
2013-08-01 16:52:25 +0000

    include numpy headers in setup.py for Mac

--- setup.py
+++ setup.py
@@ -12,9 +12,11 @@ setup(name="_transformations",
       cmdclass = {"build_ext": build_ext},
       ext_modules=[
           Extension("Domains.HIVTreatment_dynamics",
-                             ["Domains/HIVTreatment_dynamics.pyx"]),
+                             ["Domains/HIVTreatment_dynamics.pyx"],
+                             include_dirs=[numpy.get_include()]),
           Extension("Representations.kernels",
-                             ["Representations/kernels.pyx"]),
+                             ["Representations/kernels.pyx"],
+                             include_dirs=[numpy.get_include()]),
           Extension("Tools._transformations",
                              ["Tools/transformations.c"],
                              include_dirs=[numpy.get_include()])])
