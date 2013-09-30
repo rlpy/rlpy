fd72768da117e84381c131e4bf7dd7c85afa27e4
Christoph Dann <cdann@cdann.de>
2013-09-30 17:50:41 -0400

    Blocksworld cases

--- cases/blocksworld/ggq-ifdd.py
+++ cases/blocksworld/ggq-ifdd.py
@@ -7,10 +7,9 @@ from Experiments import Experiment
 import numpy as np
 from hyperopt import hp
 
-param_space = {'discretization': hp.quniform("discretization", 5, 50, 1),
-               'discover_threshold': hp.loguniform("discover_threshold",
+param_space = {'discover_threshold': hp.loguniform("discover_threshold",
                    np.log(1e-5), np.log(1e2)),
-               'lambda_': hp.uniform("lambda_", 0., 1.),
+               #'lambda_': hp.uniform("lambda_", 0., 1.),
                'boyan_N0': hp.loguniform("boyan_N0", np.log(1e1), np.log(1e5)),
                'initial_alpha': hp.loguniform("initial_alpha", np.log(5e-2), np.log(1))}
 
@@ -22,16 +21,15 @@ def make_experiment(id=1, path="./Results/Temp/{domain}/{agent}/{representation}
                     initial_alpha = .1,
                     discretization=18):
     logger = Logger()
-    max_steps = 150000
-    num_policy_checks = 30
+    max_steps = 100000
+    num_policy_checks = 20
     checks_per_policy = 1
     sparsify = 1
     ifddeps = 1e-7
     domain = BlocksWorld(blocks=6, noise=0.3, logger=logger)
-    initial_rep = IndependentDiscretization(domain, logger, discretization=discretization)
+    initial_rep = IndependentDiscretization(domain, logger)
     representation = iFDD(domain, logger, discover_threshold, initial_rep,
                           sparsify=sparsify,
-                          discretization=discretization,
                           useCache=True,
                           iFDDPlus=1-ifddeps)
     policy = eGreedy(representation, logger, epsilon=0.1)
