b2fd8f3fab7b7a0f962964b1458c56d9d7c379e6
Christoph Dann <cdann@cdann.de>
2014-01-07 18:58:44 +0100

    blocksworld poleval settings (not optimized)

---
+++ examples/blocksworld/poleval/forest1.py
@@ -0,0 +1,55 @@
+"""
+Cart-pole balancing with independent discretization
+"""
+from Tools import Logger
+from Domains import PuddleWorld
+from Agents import Q_Learning
+from Representations import *
+from Experiments import Experiment
+from hyperopt import hp
+import numpy as np
+from Domains import BlocksWorld
+from Policies import OptimalBlocksWorldPolicy
+from ValueEstimators.Forest import Forest
+from Experiments.PolicyEvaluationExperiment import PolicyEvaluationExperiment
+import os
+param_space = {"learn_rate_coef": hp.loguniform("learn_rate_coef", np.log(1e-2), np.log(1e0)),
+               'm': hp.qloguniform("m", np.log(1e0), np.log(1e2), 1),
+               'grow_coef': hp.loguniform("grow_coef", np.log(1e1), np.log(1e2)),
+               'learn_rate_exp': hp.uniform("learn_rate_exp", -0.1, -.01),
+               'grow_exp': hp.loguniform("grow_exp", np.log(1.00000001),
+                   np.log(1.4))}
+
+
+def make_experiment(id=1, path="./Results/Temp/{domain}/poleval/forest1",
+                    learn_rate_coef=0.105540,
+                    learn_rate_exp=-0.080932, #13444,  #-0.04,
+                    grow_coef=10.,
+                    grow_exp=1.0001, #0001,
+                    p_structure=0.05,
+                    m=15):
+    logger = Logger()
+    max_steps = 3000000
+
+    domain = BlocksWorld(logger=logger, gamma=0.95, blocks=6, noise=0.1)
+    pol = OptimalBlocksWorldPolicy(domain, random_action_prob=0.3)
+    forest = Forest(domain, None, num_trees=1, m=m, p_structure=p_structure,
+                    grow_coef=grow_coef, grow_exp=grow_exp, learn_rate_mode="exp",
+                    learn_rate_coef=learn_rate_coef, learn_rate_exp=learn_rate_exp,
+                    kappa=0.0001, precuts=2)
+    experiment = PolicyEvaluationExperiment(forest, domain, pol, max_steps=max_steps,
+                                            num_checks=20, path=path, id=id, log_interval=10)
+
+    return experiment
+
+if __name__ == '__main__':
+    experiment = make_experiment(1)
+    #from Tools.run import run_profiled
+    #run_profiled(make_experiment)
+    experiment.run(visualize_learning=False)
+    experiment.plot(y="rmse")
+    experiment.estimator.trees[0].save_dot(os.path.join(experiment.full_path, "Tree0.dot"))
+    #tree = experiment.estimator.trees[0]
+    #import matplotlib.pyplot as plt
+    #plt.figure("Cuts")
+    #tree.plot_2d_cuts()
---
+++ examples/blocksworld/poleval/tabular.py
@@ -0,0 +1,59 @@
+"""
+Cart-pole balancing with independent discretization
+"""
+from Tools import Logger
+from Domains import BlocksWorld
+from ValueEstimators.TDLearning import TDLearning
+from Representations import *
+from Experiments import Experiment
+from hyperopt import hp
+import numpy as np
+from Domains import PuddleWorld
+from Policies import OptimalBlocksWorldPolicy
+from ValueEstimators.Forest import Forest
+from Experiments.PolicyEvaluationExperiment import PolicyEvaluationExperiment
+
+param_space = {'lambda_': hp.uniform("lambda_", 0., 1.),
+               'boyan_N0': hp.loguniform("boyan_N0", np.log(1e1), np.log(1e5)),
+               'initial_alpha': hp.loguniform("initial_alpha", np.log(5e-2), np.log(1))}
+
+
+def make_experiment(id=1, path="./Results/Temp/{domain}/poleval/tab",
+                    boyan_N0=10000.,
+                    initial_alpha=0.1,
+                    lambda_=0.):
+    logger = Logger()
+    max_steps = 1000000
+
+    domain = BlocksWorld(blocks=6, gamma=0.95, logger=logger, noise=0.1)
+    representation = IncrementalTabular(domain, logger=logger)
+    pol = OptimalBlocksWorldPolicy(domain, random_action_prob=0.3)
+    domain.policy = pol
+    estimator = TDLearning(representation=representation, lambda_=lambda_,
+                           boyan_N0=boyan_N0, initial_alpha=initial_alpha, alpha_decay_mode="boyan")
+    experiment = PolicyEvaluationExperiment(estimator, domain, pol, max_steps=max_steps, num_checks=20,
+                                            path=path, log_interval=10, id=id)
+
+    #policy = eGreedy(representation, logger, epsilon=0.1)
+    #agent = Q_Learning(representation, policy, domain, logger
+    #                   ,lambda_=lambda_, initial_alpha=initial_alpha,
+    #                   alpha_decay_mode="boyan", boyan_N0=boyan_N0)
+    #experiment = Experiment(**locals())
+    return experiment
+
+if __name__ == '__main__':
+    experiment = make_experiment(1)
+    experiment.run(visualize_learning=False)
+    experiment.plot(y="rmse")
+    """
+    import matplotlib.pyplot as plt
+    plt.figure()
+    V_true, s_test, s_term, s_distr = experiment.domain.V(experiment.target_policy)
+    V_pred = np.zeros_like(V_true)
+    for i in xrange(s_test.shape[1]):
+        V_pred[i] = experiment.estimator.predict(s_test[:,i], s_term[i])
+    plt.plot(V_true, label="true")
+    plt.plot(V_pred, label="prediction")
+    plt.plot(s_distr / s_distr.sum(), label="distr")
+    plt.legend()
+    """
