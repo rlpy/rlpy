47044941a56dacf69397df8db9426e31633af859
Christoph Dann <cdann@cdann.de>
2014-01-20 10:24:18 +0100

    good policy for swingup with friction saved as fixed policy

--- Policies/__init__.py
+++ Policies/__init__.py
@@ -3,3 +3,4 @@ from UniformRandom import UniformRandom
 from gibbs import GibbsPolicy
 from FixedPolicy import FixedPolicy
 from FixedPolicy import BasicPuddlePolicy, OptimalBlocksWorldPolicy
+from FixedPolicy import PoleBalanceBangBangPolicy
