09882b3ebe9e84c579efd80ca5458db4f01c7921
Christoph Dann <cdann@mit.edu>
2014-01-21 15:06:35 -0500

    puddleworld 3m experiments

---
+++ examples/puddleworld/poleval/forest10_precut_3M.py
@@ -0,0 +1,57 @@
+"""
+Cart-pole balancing with independent discretization
+"""
+from Tools import Logger
+from Domains import PuddleWorld
+from Agents import Q_Learning
+from Representations import *
+from Experiments import Experiment
+from hyperopt import hp
+import numpy as np
+from Domains import PuddleWorld
+from Policies import BasicPuddlePolicy
+from ValueEstimators.Forest import Forest
+from Experiments.PolicyEvaluationExperiment import PolicyEvaluationExperiment
+
+param_space = {"learn_rate_coef": hp.loguniform("learn_rate_coef", np.log(1e-2), np.log(1e0)),
+               'm': hp.qloguniform("m", np.log(1e0), np.log(1e2), 1),
+               'grow_coef': hp.loguniform("grow_coef", np.log(1e1), np.log(1e2)),
+               'learn_rate_exp': hp.uniform("learn_rate_exp", -0.1, -.01),
+               'grow_exp': hp.loguniform("grow_exp", np.log(1.00000001),
+                   np.log(1.4))}
+
+
+def make_experiment(id=1, path="./Results/Temp/{domain}/poleval/forest10_precut",
+                    learn_rate_coef=0.1404178262,
+                    learn_rate_exp=-0.0940741, #13444,  #-0.04,
+                    grow_coef=10.5790307,
+                    grow_exp=1.02322986, #0001,
+                    p_structure=0.05,
+                    m=10,
+                    lambda_=0.8):
+    logger = Logger()
+    max_steps = 3000000
+
+    domain = PuddleWorld(logger=logger)
+    pol = BasicPuddlePolicy(domain, None)
+    forest = Forest(domain, None, num_trees=10, m=m, p_structure=p_structure,
+                    grow_coef=grow_coef, grow_exp=grow_exp, learn_rate_mode="exp",
+                    learn_rate_coef=learn_rate_coef,
+                    learn_rate_exp=learn_rate_exp,
+                    precuts=16, lambda_=lambda_)
+    experiment = PolicyEvaluationExperiment(forest, domain, pol, max_steps=max_steps,
+                                            num_checks=20, path=path, id=id, log_interval=10)
+
+    return experiment
+
+if __name__ == '__main__':
+    experiment = make_experiment(1)
+    #from Tools.run import run_profiled
+    #run_profiled(make_experiment)
+    experiment.run(visualize_learning=False)
+    #experiment.plot(y="rmse")
+
+    #tree = experiment.estimator.trees[0]
+    #import matplotlib.pyplot as plt
+    #plt.figure("Cuts")
+    #tree.plot_2d_cuts()
---
+++ examples/puddleworld/poleval/ifdd_3M.py
@@ -0,0 +1,50 @@
+from Tools import Logger
+from Domains import PuddleWorld 
+from ValueEstimators.TDLearning import TDLearning
+from Representations import *
+from Policies import eGreedy
+from Experiments.PolicyEvaluationExperiment import PolicyEvaluationExperiment
+from Policies import BasicPuddlePolicy
+import numpy as np
+from hyperopt import hp
+
+param_space = {'discover_threshold': hp.loguniform("discover_threshold",
+                   np.log(1e-3), np.log(1e2)),
+               'discretization': hp.qloguniform('discretization', np.log(5),
+                   np.log(50), 1),
+               'lambda_': hp.uniform("lambda_", 0., 1.),
+               'boyan_N0': hp.loguniform("boyan_N0", np.log(1e1), np.log(1e5)),
+               'initial_alpha': hp.loguniform("initial_alpha", np.log(1e-2), np.log(1))}
+
+
+def make_experiment(id=1, path="./Results/Temp/{domain}/poleval/ifdd/",
+                    discover_threshold=2.0953701,
+                    lambda_=0.58488,
+                    boyan_N0=40172.71,
+                    discretization=28,
+                    initial_alpha=0.17978):
+    logger = Logger()
+    max_steps = 3000000
+    sparsify = 1
+    ifddeps = 1e-6
+    domain = PuddleWorld(logger=logger)
+    pol = BasicPuddlePolicy(domain, None)
+    initial_rep = IndependentDiscretization(domain, logger,
+                                            discretization=discretization)
+    representation = iFDD(domain, logger, discover_threshold, initial_rep,
+                          sparsify=sparsify,
+                          useCache=True,
+                          iFDDPlus=1-ifddeps)
+    estimator = TDLearning(representation=representation, lambda_=lambda_,
+                           boyan_N0=boyan_N0, initial_alpha=initial_alpha, alpha_decay_mode="boyan")
+    experiment = PolicyEvaluationExperiment(estimator, domain, pol, max_steps=max_steps, num_checks=20,
+                                            path=path, log_interval=10, id=id)
+    return experiment
+
+if __name__ == '__main__':
+    from Tools.run import run_profiled
+    #run_profiled(make_experiment)
+    experiment = make_experiment(1)
+    experiment.run()
+    #experiment.plot()
+    #experiment.save()
