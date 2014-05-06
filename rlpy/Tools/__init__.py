37d69eddcfde4f8676ea6cf039e4246fea54d602
Christoph Dann <cdann@cdann.de>
2014-05-06 20:13:50 +0100

    removed reference to Merger

--- rlpy/Tools/__init__.py
+++ rlpy/Tools/__init__.py
@@ -1,5 +1,4 @@
 from .GeneralTools import *
 from .Logger import Logger
-from .Merger import Merger
 from .PriorityQueueWithNovelty import PriorityQueueWithNovelty
 from .GeneralTools import __rlpy_location__
