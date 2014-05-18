13160f2e4633bf9f6edde955c5fa6f3fb27d00e7
Bobby Klein <bobklein2@gmail.com>
2014-05-18 00:12:45 -0500

    theta->weight_vec

--- rlpy/Policies/gibbs.py
+++ rlpy/Policies/gibbs.py
@@ -29,10 +29,10 @@ class GibbsPolicy(Policy):
         n = self.representation.features_num
         phi = self.representation.phi(s, False)
         res = -np.outer(v, phi)
-        res.shape = self.representation.theta.shape
+        res.shape = self.representation.weight_vec.shape
         res[a * n:(a + 1) * n] += phi
         assert not np.any(np.isnan(res))
-        #res.shape = self.representation.theta.shape
+        #res.shape = self.representation.weight_vec.shape
         return res
 
     def prob(self, s, a):
@@ -44,17 +44,17 @@ class GibbsPolicy(Policy):
 
     @property
     def theta(self):
-        return self.representation.theta
+        return self.representation.weight_vec
 
     @theta.setter
     def theta(self, v):
-        self.representation.theta = v
+        self.representation.weight_vec = v
 
     def probabilities(self, s, terminal):
 
         phi = self.representation.phi(s, terminal)
         n = self.representation.features_num
-        v = np.exp(np.dot(self.representation.theta.reshape(-1, n), phi))
+        v = np.exp(np.dot(self.representation.weight_vec.reshape(-1, n), phi))
         v[v > 1e50] = 1e50
         r = v / v.sum()
         assert not np.any(np.isnan(r))
