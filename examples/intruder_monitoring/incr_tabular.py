df40d8b6b48a8ec49189d3f59103a1547e122750
Bobby Klein <bobklein2@gmail.com>
2014-05-18 14:18:50 -0400

    added new examples, NEED HYPEROPT

---
+++ examples/intruder_monitoring/incr_tabular.py
@@ -0,0 +1,41 @@
+"""
+Intruder Monitoring with incremental tabular representation, Q-Learning
+"""
+from rlpy.Domains.IntruderMonitoring import IntruderMonitoring
+from rlpy.Agents import Q_Learning
+from rlpy.Representations import IncrementalTabular
+from rlpy.Policies import eGreedy
+from rlpy.Experiments import Experiment
+import numpy as np
+from hyperopt import hp
+
+param_space = {'boyan_N0': hp.loguniform("boyan_N0", np.log(1e1), np.log(1e5)),
+               'initial_learn_rate': hp.loguniform("initial_learn_rate", np.log(5e-2), np.log(1))}
+
+
+def make_experiment(
+        id=1, path="./Results/Temp/{domain}/{agent}/{representation}/",
+        boyan_N0=120,
+        initial_learn_rate=.06,
+        discretization=50):
+    max_steps = 100000
+    num_policy_checks = 10
+    checks_per_policy = 1
+
+    domain = IntruderMonitoring()
+    representation = IncrementalTabular(domain)
+    policy = eGreedy(representation, epsilon=0.1)
+    agent = Q_Learning(
+        domain, policy, representation, lambda_=0.9, initial_learn_rate=initial_learn_rate,
+        learn_rate_decay_mode="boyan", boyan_N0=boyan_N0)
+    experiment = Experiment(**locals())
+    return experiment
+
+if __name__ == '__main__':
+    from rlpy.Tools.run import run_profiled
+    # run_profiled(make_experiment)
+    experiment = make_experiment(1)
+    experiment.run(visualize_steps=False, visualize_performance=10,
+                   visualize_learning=True)
+    experiment.plot()
+    # experiment.save()
---
+++ examples/intruder_monitoring/sarsa-ifdd.py
@@ -0,0 +1,49 @@
+"""
+Intruder Monitoring with iFDD representation, SARSA
+"""
+from rlpy.Domains.IntruderMonitoring import IntruderMonitoring
+from rlpy.Agents import SARSA
+from rlpy.Representations import iFDD, IndependentDiscretization
+from rlpy.Policies import eGreedy
+from rlpy.Experiments import Experiment
+import numpy as np
+from hyperopt import hp
+
+param_space = {'discover_threshold': hp.loguniform("discover_threshold",
+                                                   np.log(1e-3), np.log(1e2)),
+               'boyan_N0': hp.loguniform("boyan_N0", np.log(1e1), np.log(1e5)),
+               'initial_learn_rate': hp.loguniform("initial_learn_rate", np.log(5e-2), np.log(1))}
+
+
+def make_experiment(
+        id=1, path="./Results/Temp/{domain}/{agent}/{representation}/",
+        discover_threshold=1.0,
+        lambda_=0.,
+        boyan_N0=20.1,
+        initial_learn_rate=0.330):
+    max_steps = 100000
+    num_policy_checks = 10
+    checks_per_policy = 1
+    sparsify = 1
+    ifddeps = 1e-7
+    domain = IntruderMonitoring()
+    initial_rep = IndependentDiscretization(domain)
+    representation = iFDD(domain, discover_threshold, initial_rep,
+                          sparsify=sparsify,
+                          useCache=True,
+                          iFDDPlus=1 - ifddeps)
+    policy = eGreedy(representation, epsilon=0.1)
+    agent = SARSA(
+        domain, policy, representation, lambda_=lambda_, initial_learn_rate=initial_learn_rate,
+        learn_rate_decay_mode="boyan", boyan_N0=boyan_N0)
+    experiment = Experiment(**locals())
+    return experiment
+
+if __name__ == '__main__':
+    from rlpy.Tools.run import run_profiled
+    # run_profiled(make_experiment)
+    experiment = make_experiment(1)
+    experiment.run(visualize_steps=False, visualize_performance=10,
+                   visualize_learning=True)
+    # experiment.plot()
+    # experiment.save()
