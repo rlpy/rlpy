464f996da6f27fed6cb6c7aa31a1e825b0376211
Bobby Klein <bobklein2@gmail.com>
2014-05-18 00:13:51 -0500

    exclude tut experiment files from nosetests (user must create example domains/reps etc. themselves following tuts)

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -14,6 +14,8 @@ __license__ = "BSD 3-Clause"
 
 def test_all_100_step():
     for fn in glob.glob("examples/**/*.py"):
+        if fn in glob.glob("examples/tutorial/*"):
+            continue; # This is a tutorial file, just skip
         if "run" in fn or "plot" in fn:
             continue
         yield check_running, fn, 100
