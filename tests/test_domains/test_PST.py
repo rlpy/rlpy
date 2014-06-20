024b49de29f7bf447c05028b31711c4b11d141db
Bobby Klein <bobklein2@gmail.com>
2014-06-20 17:43:27 -0500

    added visualization randomization test

--- tests/test_domains/test_PST.py
+++ tests/test_domains/test_PST.py
@@ -1,4 +1,4 @@
-from rlpy.Representations import Tabular
+from rlpy.Representations import IncrementalTabular
 from rlpy.Domains import PST
 from rlpy.Domains.PST import UAVLocation, ActuatorState, SensorState, UAVAction
 from rlpy.Agents.TDControlAgent import SARSA
@@ -7,13 +7,76 @@ from rlpy.Tools import __rlpy_location__
 from rlpy.Tools import vec2id, id2vec
 import os
 
+from rlpy.Policies import eGreedy
+from rlpy.Experiments import Experiment
+import logging
+
+def _make_experiment(exp_id=1, path="./Results/Tmp/test_PST):
+        """
+    Each file specifying an experimental setup should contain a
+    make_experiment function which returns an instance of the Experiment
+    class with everything set up.
+
+    @param id: number used to seed the random number generators
+    @param path: output directory where logs and results are stored
+    """
+
+    ## Domain:
+    NUM_UAV=3
+    domain = PST(NUM_UAV=NUM_UAV)
+
+    ## Representation
+    # discretization only needed for continuous state spaces, discarded otherwise
+    representation  = IncrementalTabular(domain)
+
+    ## Policy
+    policy = eGreedy(representation, epsilon=0.1)
+
+    ## Agent
+    agent = SARSA(representation=representation, policy=policy,
+                  disount_factor=domain.discount_factor,
+                       learn_rate=0.1)
+    checks_per_policy = 3
+    max_steps = 50
+    num_policy_checks = 3
+    experiment = Experiment(**locals())
+    return experiment
+
+def _checkSameExperimentResults(exp1, exp2):
+    """ Returns False if experiments gave same results, true if they match. """
+    if not np.all(exp1.result["learning_steps"] == exp2.results["learning_steps"]):
+        # Same number of steps before failure (where applicable)
+        return False
+    if not np.all(exp1.result["return"] == exp2.result["return"]):
+        # Same return on each test episode
+        return False
+    if not np.all(exp1.result["steps"] == exp2.result["steps"]):
+        # Same number of steps taken on each training episode
+        return False
+    return True
+
 def test_seed():
     """ Ensure that providing the same random seed yields same result """
     # [[initialize and run experiment without visual]]
-    # [[initialize and run experiment with visual]]
-    # [[assert get same results]]
+    expNoVis = _make_experiment(exp_id=1)
+    expNoVis.run(visualize_steps=False,
+            visualize_learning=False,
+            visualize_performance=0)
     
-    # TODO get from fiftychain
+    # [[initialize and run experiment with visual]]
+    expVis1 = _make_experiment(exp_id=1)
+    expVis1.run(visualize_steps=True,
+            visualize_learning=False,
+            visualize_performance=1)
+    
+    expVis2 = _make_experiment(exp_id=1)
+    expVis2.run(visualize_steps=False,
+            visualize_learning=True,
+            visualize_performance=1)
+    
+    # [[assert get same results]]
+    assert _checkSameExperimentResults(expNoVis, expVis1)
+    assert _checkSameExperimentResults(expNoVis, expVis2)
 
 def test_errs():
     """ Ensure that we can call custom methods without error """
