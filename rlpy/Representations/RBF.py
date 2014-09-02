b91b1539d87462c804e915da792db25825e7497e
Christoph Dann <cdann@cmu.edu>
2014-09-02 17:57:21 -0400

    fixes #46, thanks pvrancx for reporting!

--- rlpy/Representations/RBF.py
+++ rlpy/Representations/RBF.py
@@ -12,13 +12,13 @@ __author__ = "Alborz Geramifard"
 
 
 class RBF(Representation):
-    """ 
+    """
     Representation that uses a weighted sum of radial basis functions (RBFs)
-    to reconstruct the value function.  Each RBF has a mean and variance 
+    to reconstruct the value function.  Each RBF has a mean and variance
     based on user-specified resolution_min, resolution_max, and grid_bins.
     See specification in __init__ below.
-    
-    
+
+
     """
     state_dimensions = None
     rbfs_mu = None  #: The mean of RBFs
@@ -29,36 +29,36 @@ class RBF(Representation):
                  const_feature=True, resolution_min=2., resolution_max=None,
                  seed=1, normalize=False, grid_bins=None, include_border=False):
         """
-        :param domain: the :py:class`~rlpy.Domains.Domain.Domain` associated 
+        :param domain: the :py:class`~rlpy.Domains.Domain.Domain` associated
             with the value function we want to learn.
         :param num_rbfs: (Optional) Number of RBFs to use for fnctn
-            approximation.  *THIS IS IGNORED* if grid_bins != None, and is 
+            approximation.  *THIS IS IGNORED* if grid_bins != None, and is
             instead determined by the resolution.
-        :param state_dimensions: (Optional) Allows user to select subset of 
+        :param state_dimensions: (Optional) Allows user to select subset of
             state dimensions for representation: ndarray.
         :param const_feature: Boolean, set true to allow for constant offset
-        :param resolution_min: If ``grid_bins`` is specified, the standard 
-            deviation sigma of each RBF is given by the average with 
-            ``resolution_max``; otherwise it is selected uniform random in the 
+        :param resolution_min: If ``grid_bins`` is specified, the standard
+            deviation sigma of each RBF is given by the average with
+            ``resolution_max``; otherwise it is selected uniform random in the
             range with resolution_max.
-        :param resolution_max: If ``grid_bins`` is specified, the standard 
-            deviation sigma of each RBF is given by the average with 
-            ``resolution_min``; otherwise it is selected uniform random in the 
+        :param resolution_max: If ``grid_bins`` is specified, the standard
+            deviation sigma of each RBF is given by the average with
+            ``resolution_min``; otherwise it is selected uniform random in the
             range with resolution_min.
         :param seed: To seed the random state generator when placing RBFs.
-        :param normalize: (Boolean) If true, normalize returned feature 
+        :param normalize: (Boolean) If true, normalize returned feature
             function values phi(s) so that sum( phi(s) ) = 1.
-        :param grid_bins: ndarray, an int for each dimension, determines 
+        :param grid_bins: ndarray, an int for each dimension, determines
             discretization of each dimension.
-        :param include_border: (Boolean) If true, adds an extra RBF to include 
+        :param include_border: (Boolean) If true, adds an extra RBF to include
             the domain boundaries.
-        
+
         """
         self.grid_bins = grid_bins
         self.resolution_max = resolution_max
         self.resolution_min = resolution_min
         self.num_rbfs = num_rbfs
-        
+
         if resolution_max is None:
             resolution_max = resolution_min
 
@@ -67,11 +67,11 @@ class RBF(Representation):
         else:  # just consider all dimensions
             state_dimensions = range(domain.state_space_dims)
             self.dims = domain.state_space_dims
-            
+
         if self.grid_bins is not None:
             # uniform grid of rbfs
-            self.rbfs_mu, self.num_rbfs = self.uniformRBFs(
-                self.grid_bins, include_border)
+            self.rbfs_mu, self.num_rbfs = self._uniformRBFs(
+                self.grid_bins, domain, include_border)
             self.rbfs_sigma = np.ones(
                 (self.num_rbfs, self.dims)) * (self.resolution_max + self.resolution_min) / 2
 
@@ -81,9 +81,9 @@ class RBF(Representation):
             self.features_num += 1  # adds a constant 1 to each feature vector
         self.state_dimensions = state_dimensions
         self.normalize = normalize
-        
+
         super(RBF, self).__init__(domain, seed)
-        
+
         self.init_randomization()
 
     def init_randomization(self):
@@ -105,7 +105,7 @@ class RBF(Representation):
                                     d] = self.random_state.uniform(
                         dim_widths[d] / self.resolution_max,
                         dim_widths[d] / self.resolution_min)
-                          
+
     def phi_nonTerminal(self, s):
         F_s = np.ones(self.features_num)
         if self.state_dimensions is not None:
@@ -123,22 +123,22 @@ class RBF(Representation):
             F_s /= F_s.sum()
         return F_s
 
-    def uniformRBFs(self, bins_per_dimension, includeBorders=False):
+    def _uniformRBFs(self, bins_per_dimension, domain, includeBorders=False):
         """
-        :param bins_per_dimension: Determines the number of RBFs to place 
+        :param bins_per_dimension: Determines the number of RBFs to place
             uniformly in each dimension, see example below.
-        :param includeBorders: (Boolean) If true, adds an extra RBF to include 
+        :param includeBorders: (Boolean) If true, adds an extra RBF to include
             the domain boundaries.
-            
+
         Positions RBF Centers uniformly across the state space.\n
-        Returns the centers as RBFs-by-dims matrix and number of rbfs. 
+        Returns the centers as RBFs-by-dims matrix and number of rbfs.
         Each row is a center of an RBF. \n
         Example: 2D domain where each dimension is in [0,3]
-        with bins = [2,3], False => we get 1 center in the first dimension and 
+        with bins = [2,3], False => we get 1 center in the first dimension and
         2 centers in the second dimension, hence the combination is:\n
         1.5    1 \n
         1.5    2 \n
-        with parameter [2,3], True => we get 3 center in the first dimension 
+        with parameter [2,3], True => we get 3 center in the first dimension
         and 5 centers in the second dimension, hence the combination is: \n
         0      0 \n
         0      1 \n
@@ -152,9 +152,8 @@ class RBF(Representation):
         3      1 \n
         3      2 \n
         3      3 \n
-        
+
         """
-        domain = self.domain
         dims = domain.state_space_dims
         if includeBorders:
             rbfs_num = np.prod(bins_per_dimension[:] + 1)
