64be0e6cbf1a45661f9342a628a77da94f127f54
Anand <anandtrex@users.noreply.github.com>
2018-02-24 00:34:33 +0100

    Removing unicode literals

--- rlpy/Domains/SystemAdministrator.py
+++ rlpy/Domains/SystemAdministrator.py
@@ -1,6 +1,5 @@
 """Network administrator task."""
 from __future__ import division
-from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 
